3948c73b3308f954362f5d66b297c13464d72d3f
Youngnam Lee <lyn00227@naver.com>
2022-11-30 20:44:50 +0900

    문의 게시글 가기 수정

--- src/main/java/kr/co/enjo2/service/qna/QnaContentService.java
+++ src/main/java/kr/co/enjo2/service/qna/QnaContentService.java
@@ -15,31 +15,27 @@ public class QnaContentService implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward forward = null;
 		try {
+			String no = request.getParameter("no");
+			QnaDao qnaDao = new QnaDao();
+			QnaDto qna = qnaDao.findOneByNo(Integer.parseInt(no));
+			// 게시글 조회수 증가
+			qnaDao.updateQnaViews(Integer.parseInt(no));
 			String userId = (String) request.getSession().getAttribute("userid");
-			if (userId == null) {
-				
-			} else {
-				// 회원인 경우
-				String no = request.getParameter("no");
-				QnaDao qnaDao = new QnaDao();
-				QnaDto qna = qnaDao.findOneByNo(Integer.parseInt(no));
-				if (qna != null) {
-					// 게시글 조회수 증가
-					qnaDao.updateQnaViews(Integer.parseInt(no));
-				}
+			if (userId != null) {
 				if (userId.equals(qna.getMemberId())) {
 					// 세션 사용자와 작성자가 같은 경우
 					request.setAttribute("mine", "true");
 				}
-				request.setAttribute("no", qna.getQnaNo());
-				request.setAttribute("title", qna.getTitle());
-				request.setAttribute("date", qna.getCreatedAt());
-				request.setAttribute("content", qna.getContent());
-				MemberDto memberDto = new MemberDao().findOne(qna.getMemberId());
-				request.setAttribute("writer", memberDto.getNickName());
-				forward = new ActionForward();
-				forward.setPath("/WEB-INF/views/member/qnaContent.jsp");
 			}
+			request.setAttribute("no", qna.getQnaNo());
+			request.setAttribute("title", qna.getTitle());
+			request.setAttribute("date", qna.getCreatedAt());
+			request.setAttribute("content", qna.getContent());
+			//MemberDto memberDto = new MemberDao().findOne(qna.getMemberId());
+			//request.setAttribute("writer", memberDto.getNickName());
+			request.setAttribute("writer", qna.getMemberId());
+			forward = new ActionForward();
+			forward.setPath("/WEB-INF/views/member/qnaContent.jsp");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
