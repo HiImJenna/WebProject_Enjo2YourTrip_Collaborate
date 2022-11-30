076fb828328f340aa3afef7e6cb5559a8dd192f3
Youngnam Lee <lyn00227@naver.com>
2022-11-30 10:53:49 +0900

    글작성 비회원 로그인 유도

--- src/main/java/kr/co/enjo2/service/notice/NoticeWrite.java
+++ src/main/java/kr/co/enjo2/service/notice/NoticeWrite.java
@@ -1,5 +1,7 @@
 package kr.co.enjo2.service.notice;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,33 +10,30 @@ import kr.co.enjo2.action.ActionForward;
 import kr.co.enjo2.dao.member.MemberDao;
 import kr.co.enjo2.dto.member.MemberDto;
 
-//서비스를 지원하는 모드 객체는 Action 인터페이스를 구현하기를 원해요
-// 당신은 ActionForward execute 반드시 구현해야 한다
 public class NoticeWrite implements Action {
-
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-
-		String nic = "게스트";
-		
+		ActionForward forward = null;
 		try {
-			
-			String id = (String)request.getSession().getAttribute("userid");
-			
-			MemberDto member = null;
-			if(id != null) {
-				member = new MemberDao().findOne(id);
-				nic = member.getNickName();
-			} 
+			String userId = (String)request.getSession().getAttribute("userid");
+			if (userId != null) {
+				// 로그인 사용자인 경우
+				forward = new ActionForward();
+				String nickName = new MemberDao().findOne(userId).getNickName();
+				forward.setRedirect(false);
+				request.setAttribute("nickName", nickName);
+				forward.setPath("/WEB-INF/views/member/noticeWrite.jsp");
+			}
+			else {
+				// 비회원인 경우 (공지글을 작성할 수 없음)
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				//out.println("<script>alert('로그인이 필요한 서비스 입니다.'); history.go(-1);</script>");
+				out.println("<script>const result = confirm('로그인이 필요한 서비스입니다. 로그인 페이지로 이동하시겠습니까?'); if(result){location.href=\"/loginView.do\"}else{history.go(-1);}</script>");
+			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		ActionForward action = new ActionForward();
-		request.setAttribute("nickName", nic);
-		action.setRedirect(false);
-		action.setPath("/WEB-INF/views/member/noticeWrite.jsp");
-		return action;
+		return forward;
 	}
-	
 }
